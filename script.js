56f3fd774a2e5c20d2b5a13a8c5a66824b7e2265
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-08-13 21:47:12 +0300

    saving work

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-let money = 0;
-let currentCurrency = "euro";
+let money = localStorage.getItem("money") || 0;
+console.log(money);
+
+let currentCurrency = "dollar";
+let symbol = "$";
+let exchangeRate = 1;
 const entries = [];
 
 const display = document.getElementById("budget-display");
@@ -33,14 +37,9 @@ function createEntry(type){
     console.log("create entry executed");
     
     let value = Number(document.getElementById("amount").value);
-    console.log(value);
-    if(currentCurrency === "euro"){
-        value = value / 0.8560;
-        console.log(value + " in Dollars");
-        
-    } else{
-        value = value * 1;
-    }
+    checkCurrency();
+    value /= exchangeRate;
+    console.log(value, symbol);
     
     let reason = String(document.getElementById("reason").value);
     console.log(reason);
@@ -54,36 +53,26 @@ function createEntry(type){
     
     updateTotal(value, type);
     closeMenu();
-    displayEntries();
 }
 
 function updateTotal(value, type){
-    let exchangeRate = 1;
-    switch(currentCurrency){
-        case"dollar":
-            exchangeRate = 1;
-            break;
-        case "euro":
-            exchangeRate = 0.8560;
-            break;
-    }
-    
     switch(type){
         case "expense":
-            money-=value*exchangeRate;
+            money-=value;
             break;
         case"add":
-            money+=value*exchangeRate;
+            money+=value;
             break;
         default:
             console.log("No Type preovided");
-            return;
             
     }
 
     console.log(money, currentCurrency);
     
-    display.textContent=`$${money.toFixed(2)}` 
+    display.innerHTML = `${symbol}${money.toFixed(2)*exchangeRate.toFixed(2)}`;
+
+    displayEntries();
 }
 
 class Entry{
@@ -98,20 +87,39 @@ function displayEntries(){
     const entryDisplay = document.querySelector("#Entries");
     let html="";
     entries.forEach(entry => {
+        const entryValue = entry.value*exchangeRate;
         if(entry.type === "add"){
             html += `<div class="entry col-8 mx-auto bg-light rounded shadow p-3 mt-2">
                     <h5>Reason: ${entry.reason}</h5>
-                    <h5 class="entry-sum">+$${entry.value.toFixed(2)}</h5>
+                    <h5 class="entry-sum">+${symbol}${entryValue.toFixed(2)}</h5>
                 </div>`
         } else if(entry.type === "expense"){
             html += `<div class="entry col-8 mx-auto bg-light rounded shadow p-3 mt-2">
                     <h5>Reason: ${entry.reason}</h5>
-                    <h5 class="entry-sum">-$${entry.value.toFixed(2)}</h5>
+                    <h5 class="entry-sum">-${symbol}${entryValue.toFixed(2)}</h5>
                 </div>`
         }
     })
     entryDisplay.innerHTML = html;
 }
 
+function changeCurrency(){
+    if (currentCurrency ==="euro"){
+        currentCurrency = "dollar";
+    } else{
+        currentCurrency = "euro";
+    }
+    checkCurrency();
+    updateTotal(0, "");
+}
 
+function checkCurrency(){
+    if(currentCurrency ==="euro"){
+        exchangeRate = 0.8560;
+        symbol = "€";
+    } else{
+        exchangeRate = 1;
+        symbol = "$";
+    }
+}
 //code need to be optimised, polish the function about changing the value
