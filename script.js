3b07db4932f724d4cc0945c4f88e363158adec6b
Zdravko Georgiev <zzgzdravko@gmail.com>
2025-08-14 19:48:15 +0300

    working logic

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let money = localStorage.getItem("money") || 0;
+// localStorage.clear();
+let money = Number(localStorage.getItem("money")) || 0;
 console.log(money);
 
-let currentCurrency = "dollar";
+let currentCurrency = localStorage.getItem("currentCurrency") || "dollar";
 let symbol = "$";
 let exchangeRate = 1;
-const entries = [];
+const entries = JSON.parse(localStorage.getItem("entries")) || [];
+
+window.addEventListener("DOMContentLoaded", () => {
+    checkCurrency();
+    updateTotal(0, "");
+})
 
 const display = document.getElementById("budget-display");
 
@@ -38,6 +44,7 @@ function createEntry(type){
     
     let value = Number(document.getElementById("amount").value);
     checkCurrency();
+
     value /= exchangeRate;
     console.log(value, symbol);
     
@@ -46,13 +53,38 @@ function createEntry(type){
 
     console.log(type);
     
-    
-
     entries.push(new Entry(type, value, reason));
+    localStorage.setItem("entries", JSON.stringify(entries));
+
     console.log(entries);
     
     updateTotal(value, type);
     closeMenu();
+}
+
+function loadEntries() {
+  return JSON.parse(localStorage.getItem("entries") || "[]", (key, value) => {
+    if (key === "date") return new Date(value); // revive into Date
+    return value;
+  });
+}
+
+function removeEntry(index){
+    const removingValue = entries[index].value;
+    let entryType = entries[index].type;
+    console.log(entryType);
+    
+
+    entries.splice(index, 1);
+    localStorage.setItem("entries", JSON.stringify(entries));
+
+    if(entryType === "expense"){
+        entryType = "add";
+    } else{
+        entryType = "expense";
+    }
+
+    updateTotal(removingValue, entryType);
 }
 
 function updateTotal(value, type){
@@ -69,8 +101,10 @@ function updateTotal(value, type){
     }
 
     console.log(money, currentCurrency);
+    localStorage.setItem("money", String(money.toFixed(2)));
+
     const displayMoney = `${symbol}${(money*exchangeRate).toFixed(2)}`
-    display.innerHTML = displayMoney;//doesnt work properly more work needed
+    display.innerHTML = displayMoney;
 
     displayEntries();
 }
@@ -80,27 +114,40 @@ class Entry{
         this.type = type,
         this.value = value,
         this.reason = reason
+        this.date = new Date().toISOString();
     }
 }
 
-function displayEntries(){
+function displayEntries() {
+    let index = 0;
     const entryDisplay = document.querySelector("#Entries");
-    let html="";
-    entries.forEach(entry => {
-        const entryValue = entry.value*exchangeRate;
-        if(entry.type === "add"){
-            html += `<div class="entry col-8 mx-auto bg-light rounded shadow p-3 mt-2">
+    let html = "";
+    const displayingEntries = loadEntries();
+    if (displayingEntries) {
+        displayingEntries.forEach(entry => {
+            const entryValue = entry.value * exchangeRate;
+            const entryDate = entry.date;
+            if (entry.type === "add") {
+                html += `<div class="entry col-8 mx-auto bg-light rounded shadow p-3 mt-2">
+                    <h6>Date: ${entryDate.getDate()}.${Number(entryDate.getMonth()) + 1}.${entryDate.getFullYear()}</h6>
                     <h5>Reason: ${entry.reason}</h5>
                     <h5 class="entry-sum">+${symbol}${entryValue.toFixed(2)}</h5>
+                    <button onclick="removeEntry(${index})">Delete</button>
                 </div>`
-        } else if(entry.type === "expense"){
-            html += `<div class="entry col-8 mx-auto bg-light rounded shadow p-3 mt-2">
+            } else if (entry.type === "expense") {
+                html += `<div class="entry col-8 mx-auto bg-light rounded shadow p-3 mt-2">
+                    <h6>Date: ${entryDate.getFullYear()}/${entryDate.getMonth()}/${entryDate.getDate()}</h6>
                     <h5>Reason: ${entry.reason}</h5>
                     <h5 class="entry-sum">-${symbol}${entryValue.toFixed(2)}</h5>
+                    <button onclick="removeEntry(${index})">Delete</button>
                 </div>`
-        }
-    })
-    entryDisplay.innerHTML = html;
+            }
+
+            index++;
+        })
+        entryDisplay.innerHTML = html;
+        console.log("index ", index);
+    }
 }
 
 function changeCurrency(){
@@ -109,6 +156,8 @@ function changeCurrency(){
     } else{
         currentCurrency = "euro";
     }
+
+    localStorage.setItem("currentCurrency", currentCurrency);
     checkCurrency();
     updateTotal(0, "");
 }
